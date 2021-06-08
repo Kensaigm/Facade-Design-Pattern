28c7bc4aac9bbbbd725b4967a0c06c09dff424b2
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-07 18:01:25 -0700

    Added SingletonDerby zip to library and corrected some output text.

--- src/net/kensaigm/dp/JdbcDemo.java
+++ src/net/kensaigm/dp/JdbcDemo.java
@@ -1,7 +1,5 @@
 package net.kensaigm.dp;
 
-import net.kensaigm.dp.DbSingleton;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,7 +7,7 @@ import java.sql.Statement;
 
 public class JdbcDemo {
     public static void main(String[] args) {
-        DbSingleton instance = DbSingleton.getInstance();
+        net.kensaigm.dp.DbSingleton instance = net.kensaigm.dp.DbSingleton.getInstance();
 
         try {
             Connection conn = instance.getConnection();
@@ -23,14 +21,15 @@ public class JdbcDemo {
             statement = conn.createStatement();
             count = statement.executeUpdate("INSERT INTO Address (ID, StreetName, City, State, Zip) "
             + "VALUES (1, '42 Main Street', 'Sandwich', 'IL', '60548')");
-            System.out.println(count + "record(s) created.");
+            System.out.println(count + " record(s) created.");
             statement.close();
 
             statement = conn.createStatement();
             ResultSet rs = statement.executeQuery("SELECT * FROM Address");
 
             while(rs.next()) {
-                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3)
+                + " " + rs.getString(4) + " " + rs.getString(5));
             }
 
         } catch (SQLException e){
